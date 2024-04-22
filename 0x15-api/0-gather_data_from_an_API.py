c23cafacd58de6cdb1d515296cec3cd8b8c2e104
Nidal-h <hadrou.nidal@gmail.com>
2024-04-22 15:17:04 -0700

    0x15api second try

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,25 +7,24 @@ import sys
 USER_BASE_URL = "https://jsonplaceholder.typicode.com/users/"
 TODOS_BASE_URL = "https://jsonplaceholder.typicode.com/todos?userId="
 
-def get_todos(employee_id):
+def  get_todos(employee_id):
     # get users infos
     response = requests.get(USER_BASE_URL + str(employee_id))
     user = json.loads(response.content)
-    user_name = user['name']
+    name = user['name']
 
     # get user's todos
     response = requests.get(TODOS_BASE_URL + str(employee_id))
     todos = json.loads(response.content)
 
-    number_of_done_tasks = 0
-    number_of_tasks = len(todos)
+    done_tasks = 0
+    tasks = len(todos)
     done_tasks_titles = []
     for todo in todos:
         if todo['completed']:
             number_of_done_tasks += 1
             done_tasks_titles.append(todo['title'])
-    print('Employee %s is done with tasks(%d/%d):' 
-          % (user_name, number_of_done_tasks, number_of_tasks))
+    print('Employee %s is done with tasks(%d/%d):' % (name, done_tasks, tasks))
     for title in done_tasks_titles:
         print('\t', title)
 
