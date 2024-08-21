ba07558ba1f2b5d215c558be4803de581ec678e4
Nidal-h <hadrou.nidal@gmail.com>
2024-08-21 14:56:04 -0700

    0x1B-web_stack_debugging_4

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,13 @@ def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
     import requests
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
+                            headers=headers,
                             allow_redirects=False)
     if sub_info.status_code >= 300:
-        return 0
+        return sub_info.status_code
 
     return sub_info.json().get("data").get("subscribers")
